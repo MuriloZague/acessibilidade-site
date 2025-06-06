47df179bf8958a0c13d9627310f9ec6bd731ba2f
MuriloZague <zaguemurilo7@gmail.com>
2025-06-06 20:12:36 -0300

    feat: font ajustments added (o mais nas coxa possível)

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function toggleContrast() {
+const toggleContrast = () => {
   const body = document.body;
   const currentTheme = body.getAttribute("data-theme");
   if (currentTheme === "high-contrast") {
@@ -7,3 +7,28 @@ function toggleContrast() {
     body.setAttribute("data-theme", "high-contrast");
   }
 }
+
+let tamanhoFonteAtual = 120; // porcentagem (100% é o padrão)
+const passo = 10; // incremento em %
+
+function ajustarFonte() {
+  document.querySelector('main').style.fontSize = `${tamanhoFonteAtual}%`;
+
+}
+
+function aumentarFonte() {
+  if (tamanhoFonteAtual < 200) { // limite superior
+    tamanhoFonteAtual += passo;
+    ajustarFonte();
+  }
+}
+
+function diminuirFonte() {
+  if (tamanhoFonteAtual > 50) { // limite inferior
+    tamanhoFonteAtual -= passo;
+    ajustarFonte();
+  }
+}
+
+// Ajusta ao carregar a página
+window.onload = ajustarFonte;
